426693ee94534afadcac2ee474e3abf4b29a6f3a
TheLoneTec <kaceysherrard@hotmail.com>
2025-10-11 10:24:32 +0100

    Project Setup

---
+++ source/HSK-Storage-Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HSK-Storage-Extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HSK-Storage-Extensions")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("358a930c-c8ba-4c4d-8b2c-cf9f52dc3975")]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+
